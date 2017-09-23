2e6b28844f7bf3d692263fc017b1c9efc9e291f3
Ivan Velarde <u201521409@upc.edu.pe>
2017-09-23 14:36:01 -0500

    Actualizacion con pagina de login

---
+++ app/src/main/java/pe/edu/upc/collectionmanagement/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.collectionmanagement.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pe.edu.upc.collectionmanagement.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
